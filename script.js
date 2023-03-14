b994d9d3810eea128296fbd23f3f22c3acb918f9
matpakla <matpakla@gmail.com>
2023-03-14 13:04:11 +0100

    zadanie z egzaminu

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+const blok = document.querySelector("#blok-lewy")
+const wynik = document.createElement("p")
+blok.append(wynik)
+
 function btn() {
-    const rodzaj = document.querySelector("#rodzaj")
-    const ile = document.querySelector("#ile")
+    const rodzaj = Number(document.querySelector("#rodzaj").value)
+    const ile = Number(document.querySelector("#ile").value)
     let koszt
 
-    switch (rodzaj) {
-        case(1): {
+    if (rodzaj === 1)
             koszt = ile * 4
-        }
-        case(2): {
+    else if (rodzaj === 2)
             koszt = ile * 3.5
-        }
-        default: {
-            koszt = 0
-        }
-    }
+    else koszt = 0
+    wynik.innerHTML = `Koszt paliwa: ${koszt}zł`
 }
